87a2c4e99b61e2b19c205545d0a737f387f29a09
Yamir <luiskitikazis2@gmail.com>
2024-05-24 15:24:30 -0500

    Add .vs directory to .gitignore

---
+++ CapaPresentacionPancho/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CapaEntidad;
+using CapaNegocio;
+
+namespace CapaPresentacionPancho.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarUsuarios()
+        {
+            List<Usuario> oLista = new CN_Usuarios().Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarUsuario(Usuario objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+            if (objeto.IDUsuario == 0)
+            {
+                resultado = new CN_Usuarios().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Usuarios().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
