5f70f3fc67cc5c0892ecda5ead138c710b982cbf
Yamir <luiskitikazis2@gmail.com>
2024-06-02 20:38:28 -0500

    coreccion de login y olvide mi correo- mayus y minus perfecto

--- CapaPresentacionPancho/Controllers/AccesoController.cs
+++ CapaPresentacionPancho/Controllers/AccesoController.cs
@@ -16,96 +16,58 @@ namespace CapaPresentacionPancho.Controllers
         {
             return View();
         }
+
         public ActionResult CambiarClave()
         {
             return View();
         }
-     
+        public ActionResult Reestablecer()
+        {
+            return View();
+        }
 
 
-        //index login
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        //index login 
         [HttpPost]
         public ActionResult Index(string correo, string clave)
         {
-            Usuario oUsuario = new Usuario();
+           
+                Usuario oUsuario = new Usuario();
 
-            // Convertir el correo a mini
-            string correoLower = correo.ToLower();
+                // Convertir el correo a minúsculas
+                string correoLower = correo.ToLower();
 
-            // Buscar el usuario ignorando maryus/mini en el correo
-            oUsuario = new CN_Usuarios().Listar().Where(u => u.Correo.ToLower() == correoLower && u.Clave == CN_Recursos.ConvertirSha256(clave)).FirstOrDefault();
+                // Buscar el usuario ignorando mayúsculas/minúsculas en el correo
+                oUsuario = new CN_Usuarios().Listar().Where(u => u.Correo.ToLower() == correoLower && u.Clave == CN_Recursos.ConvertirSha256(clave)).FirstOrDefault();
 
-            if (oUsuario == null)
-            {// envia a la misma vista que es index
-                ViewBag.Error = "Correo o contraseña no correcta";
-                return View();
-            }
-            else
-            {
-                if (oUsuario.Reestablecer)
-                {
-                    // guardar informacion y compartir de multiples vistas que estan dentro del mismo contralador
-                    TempData["IdUsuario"] = oUsuario.IDUsuario;
-                    return RedirectToAction("CambiarClave");
+                if (oUsuario == null)
+                {// envia a la misma vista que es index
+                    ViewBag.Error = "Correo o contraseña no correcta";
+                    return View();
                 }
+                else
+                {
+                    if (oUsuario.Reestablecer)
+                    {
+                        // guardar informacion y compartir de multiples vistas que estan dentro del mismo contralador
+                        TempData["IdUsuario"] = oUsuario.IDUsuario;
+                        return RedirectToAction("CambiarClave");
+                    }
 
-                FormsAuthentication.SetAuthCookie(oUsuario.Correo, false);
+                    FormsAuthentication.SetAuthCookie(oUsuario.Correo, false);
 
-                ViewBag.Error = null;
+                    ViewBag.Error = null;
 
-                return RedirectToAction("Index", "Home");
-            }
-        }
-
-
-
-        //rrestablecer contrañsea usuario
-        [HttpPost]
-        public ActionResult Reestablecer(string correo)
-        {
-
-            Usuario ousurio = new Usuario();
-
-            ousurio = new CN_Usuarios().Listar().Where(item => item.Correo == correo).FirstOrDefault();
-
-            if (ousurio == null)
-            {
-
-
-                ViewBag.Error = "No se encontro un usuario relacionado a ese correo";
-                return View();
-            }
-
-
-            string mensaje = string.Empty;
-            bool respuesta = new CN_Usuarios().ReestablecerClave(ousurio.IDUsuario, correo, out mensaje);
-
-            if (respuesta)
-            {
-
-                ViewBag.Error = null;
-                return RedirectToAction("Index", "Acceso");
-
-            }
-            else
-            {
-
-                ViewBag.Error = mensaje;
-                return View();
-            }
-
-
-
-        }
-
-
-
-
+                    return RedirectToAction("Index", "Home");
+                }
+        }//>correcion mayusculas y minusculas
 
 
         //cambiar clave usuario el admin
-
-        [HttpPost]
+        [HttpPost] 
         public ActionResult CambiarClave(string idusuario, string claveactual, string nuevaclave, string confirmarclave)
         {
 
@@ -116,8 +78,7 @@ namespace CapaPresentacionPancho.Controllers
             if (oUsuario.Clave != CN_Recursos.ConvertirSha256(claveactual))
             {
                 TempData["IdUsuario"] = idusuario;
-                //almacenar valores como cadena de texto
-                ViewData["vclave"] = "";   // confimar si la clave no es correcta
+                ViewData["vclave"] = "";
                 ViewBag.Error = "La contraseña actual no es correcta";
                 return View();
             }
@@ -125,7 +86,7 @@ namespace CapaPresentacionPancho.Controllers
             {
 
                 TempData["IdUsuario"] = idusuario;
-                ViewData["vclave"] = claveactual; //confirmacas la claves 
+                ViewData["vclave"] = claveactual;
                 ViewBag.Error = "Las contraseñas no coinciden";
                 return View();
             }
@@ -154,7 +115,44 @@ namespace CapaPresentacionPancho.Controllers
             }
 
         }
-        
+
+
+        //rrestablecer contrañsea usuario
+        [HttpPost]
+        public ActionResult Reestablecer(string correo)
+        {
+
+            Usuario ousurio = new Usuario();
+
+            // Convertir el correo a minúsculas
+            string correoLower = correo.ToLower();
+
+            // Buscar el usuario ignorando mayúsculas/minúsculas en el correo
+            ousurio = new CN_Usuarios().Listar().Where(item => item.Correo.ToLower() == correoLower).FirstOrDefault();
+
+            if (ousurio == null)
+            {
+                ViewBag.Error = "No se encontró un usuario relacionado a ese correo";
+                return View();
+            }
+
+            string mensaje = string.Empty;
+            bool respuesta = new CN_Usuarios().ReestablecerClave(ousurio.IDUsuario, correo, out mensaje);
+
+            if (respuesta)
+            {
+                ViewBag.Error = null;
+                return RedirectToAction("Index", "Acceso");
+            }
+            else
+            {
+                ViewBag.Error = mensaje;
+                return View();
+            }
+        } //correcion mayusculas y minusculas
+
+
+
         //cerrar session
         public ActionResult CerrarSesion()
         {
